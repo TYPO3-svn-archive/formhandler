10a3758a3f054766d235f3a27f167e747ae59ce7
erep <erep@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-04 20:44:16 +0000

    - FIX: turn into uncached plugin again
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandler/trunk@28375 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,5 +4,5 @@ if(!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_formhandler_pi1.php', '_pi1', 'list_type', 1);
+t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_formhandler_pi1.php', '_pi1', 'list_type', 0);
 ?>
