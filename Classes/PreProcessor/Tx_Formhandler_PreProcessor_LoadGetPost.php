2b6d584207be2b1d458375054e00bfa43626e5c4
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-11 13:29:46 +0000

    - FIX #8160: Preprocessor LoadGetPost doesn't load GPvars
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandler/trunk@34190 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/PreProcessor/Tx_Formhandler_PreProcessor_LoadGetPost.php
+++ Classes/PreProcessor/Tx_Formhandler_PreProcessor_LoadGetPost.php
@@ -34,8 +34,7 @@ class Tx_Formhandler_PreProcessor_LoadGetPost extends Tx_Formhandler_AbstractPre
 		$this->formValuesPrefix = Tx_Formhandler_Globals::$formValuesPrefix;
 		
 		$loadedGP = $this->loadGP();
-		
-		$this->gp = array_merge($loadedGP, $gp);
+		$this->gp = array_merge($loadedGP, $this->gp);
 		
 		
 		return $this->gp;
