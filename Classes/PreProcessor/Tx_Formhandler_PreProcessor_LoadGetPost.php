522c64419db421ff18d7340af9aedd7687d2630b
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-11 14:44:34 +0000

    - Removed PreProcessor_Default, since it had no prupose at all (fixes #11669)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandler/trunk@42004 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/PreProcessor/Tx_Formhandler_PreProcessor_LoadGetPost.php
+++ Classes/PreProcessor/Tx_Formhandler_PreProcessor_LoadGetPost.php
@@ -11,7 +11,7 @@
  * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
  * Public License for more details.                                       *
  *
- * $Id: Tx_Formhandler_PreProcessor_Default.php 22614 2009-07-21 20:43:47Z fabien_u $
+ * $Id: Tx_Formhandler_PreProcessor_LoadGetPost.php 22614 2009-07-21 20:43:47Z fabien_u $
  *                                                                        */
 
 /**
