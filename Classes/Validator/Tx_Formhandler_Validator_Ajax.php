8b47f728dbb7f2e69aaafa9da6ef3c3de082d6fe
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-21 08:14:12 +0000

    Do not loop through every field in AJAX validation, when only a single field is checked (#27581)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandler/trunk@48950 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Validator/Tx_Formhandler_Validator_Ajax.php
+++ Classes/Validator/Tx_Formhandler_Validator_Ajax.php
@@ -51,85 +51,76 @@ class Tx_Formhandler_Validator_Ajax extends Tx_Formhandler_AbstractValidator {
 				$restrictErrorChecks = t3lib_div::trimExplode(',', $this->settings['restrictErrorChecks']);
 			}
 
-			//foreach configured form field
-			foreach ($this->settings['fieldConf.'] as $fieldName => $fieldSettings) {
-				$name = str_replace('.', '', $fieldName);
+			$fieldSettings = $this->settings['fieldConf.'][$field . '.'];
 
-				//parse error checks
-				if (is_array($fieldSettings['errorCheck.'])) {
-					$counter = 0;
-					$errorChecks = array();
+			//parse error checks
+			if (is_array($fieldSettings['errorCheck.'])) {
+				$counter = 0;
+				$errorChecks = array();
 
-					//set required to first position if set
-					foreach ($fieldSettings['errorCheck.'] as $key => $check) {
-						if (!strstr($key, '.')) {
-							if (!strcmp($check, 'required') || !strcmp($check, 'file_required')) {
-								$errorChecks[$counter]['check'] = $check;
-								unset($fieldSettings['errorCheck.'][$key]);
-								$counter++;
-							}
-						}
-					}
-
-					//set other errorChecks
-					foreach ($fieldSettings['errorCheck.'] as $key=>$check) {
-						if (!strstr($key, '.')) {
+				//set required to first position if set
+				foreach ($fieldSettings['errorCheck.'] as $key => $check) {
+					if (!strstr($key, '.')) {
+						if (!strcmp($check, 'required') || !strcmp($check, 'file_required')) {
 							$errorChecks[$counter]['check'] = $check;
-							if (is_array($fieldSettings['errorCheck.'][$key . '.'])) {
-								$errorChecks[$counter]['params'] = $fieldSettings['errorCheck.'][$key . '.'];
-							}
+							unset($fieldSettings['errorCheck.'][$key]);
 							$counter++;
 						}
 					}
+				}
 
-					$checkFailed = '';
-					if (!isset($disableErrorCheckFields) || !in_array($name, $disableErrorCheckFields)) {
-							
-						//foreach error checks
-						foreach ($errorChecks as $idx => $check) {
-							$classNameFix = ucfirst($check['check']);
-							$errorCheckObject = $this->componentManager->getComponent('Tx_Formhandler_ErrorCheck_' . $classNameFix);
-							if ($name === $field && (empty($restrictErrorChecks) || in_array($check['check'], $restrictErrorChecks))) {
-								$gp = array($field=>$value);
-								$checkFailed = $errorCheckObject->check($check, $name, $gp);
-								if (strlen($checkFailed) > 0) {
-									if (!is_array($errors[$name])) {
-										$errors[$name] = array();
-									}
-									array_push($errors[$name], $checkFailed);
+				//set other errorChecks
+				foreach ($fieldSettings['errorCheck.'] as $key => $check) {
+					if (!strstr($key, '.')) {
+						$errorChecks[$counter]['check'] = $check;
+						if (is_array($fieldSettings['errorCheck.'][$key . '.'])) {
+							$errorChecks[$counter]['params'] = $fieldSettings['errorCheck.'][$key . '.'];
+						}
+						$counter++;
+					}
+				}
+
+				$checkFailed = '';
+				if (!isset($disableErrorCheckFields) || !in_array($field, $disableErrorCheckFields)) {
+
+					//foreach error checks
+					foreach ($errorChecks as $idx => $check) {
+						$classNameFix = ucfirst($check['check']);
+						$errorCheckObject = $this->componentManager->getComponent('Tx_Formhandler_ErrorCheck_' . $classNameFix);
+						if (empty($restrictErrorChecks) || in_array($check['check'], $restrictErrorChecks)) {
+							$gp = array($field => $value);
+							$checkFailed = $errorCheckObject->check($check, $field, $gp);
+							if (strlen($checkFailed) > 0) {
+								if (!is_array($errors[$field])) {
+									$errors[$field] = array();
 								}
+								array_push($errors[$field], $checkFailed);
 							}
 						}
 					}
 				}
 			}
 		}
+		$returnCode = '';
 		if (empty($errors)) {
 			
 			$okImg = Tx_Formhandler_StaticFuncs::getSingle($this->settings['ajax.']['config.'], 'ok');
 			if(strlen($okImg) === 0) {
 				$okImg = '<img src="' . t3lib_extMgm::extRelPath('formhandler') . 'Resources/Images/ok.png' . '" />';
 			}
-			return $okImg;
+			$returnCode = $okImg;
 		} else {
 			
 			$notOkImg = Tx_Formhandler_StaticFuncs::getSingle($this->settings['ajax.']['config.'], 'notOk');
 			if(strlen($notOkImg) === 0) {
 				$notOkImg = '<img src="' . t3lib_extMgm::extRelPath('formhandler') . 'Resources/Images/notok.png' . '" />';
 			}
-			return $notOkImg;
+			$returnCode = $notOkImg;
 		}
+		return $returnCode;
 	}
 
 	public function loadConfig() {
-		if(!Tx_Formhandler_Globals::$session) {
-			$ts = $GLOBALS['TSFE']->tmpl->setup['plugin.']['Tx_Formhandler.']['settings.'];
-			$sessionClass = 'Tx_Formhandler_Session_PHP';
-			if($ts['session.']) {
-				$sessionClass = Tx_Formhandler_StaticFuncs::prepareClassName($ts['session.']['class']);
-			}
-			Tx_Formhandler_Globals::$session = $this->componentManager->getComponent($sessionClass);
-		}
 		$tsConfig = Tx_Formhandler_Globals::$session->get('settings');
 		$this->settings = array();
 		if ($tsConfig['validators.']) {
