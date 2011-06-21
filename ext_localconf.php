e18b1496e5a1c900b574d4c596724c58d7048219
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-21 10:20:21 +0000

    Follow-Up to "Cache Component manager search results". Here comes the ext_localconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandler/trunk@48963 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,6 +9,9 @@ t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_formhandler_pi1.php', '_pi1', 
 //Hook in tslib_content->stdWrap
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['stdWrap'][$_EXTKEY] = 'EXT:formhandler/Resources/PHP/Hooks/class.tx_formhandler_stdwrap.php:tx_formhandler_stdwrap';
 
+//Delete cache file on "clear cache" command
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][$_EXTKEY] = 'EXT:formhandler/Resources/PHP/Hooks/class.tx_formhandler_clearCache.php:tx_formhandler_clearCache->clearCache';
+
 $TYPO3_CONF_VARS['FE']['eID_include']['formhandler'] = 'EXT:formhandler/Classes/Utils/Tx_Formhandler_Utils_AjaxValidate.php';
 $TYPO3_CONF_VARS['FE']['eID_include']['formhandler-removefile'] = 'EXT:formhandler/Classes/Utils/Tx_Formhandler_Utils_AjaxRemoveFile.php';
 ?>
