0590e4f63d42785b259b05636aa067b409921ea0
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-09 15:42:20 +0000

    Do not store all predefined forms in the session, but only the current one.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandler/trunk@50810 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/Tx_Formhandler_AbstractController.php
+++ Classes/Controller/Tx_Formhandler_AbstractController.php
@@ -145,7 +145,7 @@ abstract class Tx_Formhandler_AbstractController extends Tx_Formhandler_Abstract
 		$settings = $this->configuration->getSettings();
 		if ($this->predefined && is_array($settings['predef.'][$this->predefined])) {
 			$predefSettings = $settings['predef.'][$this->predefined];
-			unset($settings['predef.'][$this->predefined]);
+			unset($settings['predef.']);
 			$settings = t3lib_div::array_merge_recursive_overrule($settings, $predefSettings);
 		}
 		return $settings;
