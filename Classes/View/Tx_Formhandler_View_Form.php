c17c92efaaaffa1685982e4135ea7717521f32b5
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-02 10:03:58 +0000

    ISSET subparts failed with more than two conditions (fixes #33579)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandler/trunk@57118 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/View/Tx_Formhandler_View_Form.php
+++ Classes/View/Tx_Formhandler_View_Form.php
@@ -202,19 +202,25 @@ class Tx_Formhandler_View_Form extends Tx_Formhandler_AbstractView {
 
 		// recurse if there are more
 		if ( preg_match($pattern, $conditionValue, $matches) ){
-			$isset = $this->keyIsset($matches[2]);
-			if ($matches[3] == '||' && $isset) {
-				$return = TRUE;
-			} elseif ($matches[3] == '||' && !$isset) {
-				$return = $this->markersCountAsSet($matches[4]);
-			} elseif ($matches[3] == '&&' && $isset) {
-				$return = $this->markersCountAsSet($matches[4]);
-			} elseif ($matches[3] == '&&' && !$isset) {
-				$return = FALSE;
-			} elseif ($matches[6] == '!' && !$isset) {
-				$return = !$this->keyIsset($matches[7]);
-			} elseif ($this->globals->getSession()->get('debug')) {
-				$this->utilityFuncs->debugMessage('invalid_isset', array($matches[2]), 2);
+			$foundFields = array($matches[2], $matches[4]);
+			$operator = $matches[3];
+			foreach($foundFields as $field) {
+				$isset = $this->keyIsset($field);
+				if(strpos($field, '||') !== FALSE && !$isset) {
+					$return = $this->markersCountAsSet($field);
+				} if(strpos($field, '&&') !== FALSE && !$isset) {
+					$return = $this->markersCountAsSet($field);
+				} elseif ($operator === '||' && $isset) {
+					$return = TRUE;
+				} elseif ($operator === '&&' && $isset) {
+					$return = $this->markersCountAsSet($matches[4]);
+				} elseif ($operator === '&&' && !$isset) {
+					return FALSE;
+				} elseif ($matches[6] == '!' && !$isset) {
+					$return = !$this->keyIsset($matches[7]);
+				} elseif ($this->globals->getSession()->get('debug')) {
+					$this->utilityFuncs->debugMessage('invalid_isset', array($field), 2);
+				}
 			}
 		} else {
 
@@ -240,16 +246,18 @@ class Tx_Formhandler_View_Form extends Tx_Formhandler_AbstractView {
 		$key = str_replace(array(':', '[', ']'), array('|', '|', ''), $key);
 
 		if (!strpos($key, '|')) {
-			return !empty($this->gp[$key]);
+			$value = trim($this->gp[$key]);
+			return !empty($value);
 		}
 
 		$keys = explode('|', $key);
 
 		$tmp = $this->gp[array_shift($keys)];
 		foreach ($keys as $idx => $key) {
-			if (empty($tmp[$key])) {
+			$value = trim($tmp[$key]);
+			if (empty($value)) {
 				return FALSE;
-			}else {
+			} else {
 				$tmp = $tmp[$key];
 			}
 		}
