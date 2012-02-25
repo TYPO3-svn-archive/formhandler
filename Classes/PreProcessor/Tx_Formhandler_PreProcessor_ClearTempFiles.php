f1d25193429d6c0da4073a2057838ef66a1c2683
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-25 18:55:18 +0000

    Allow all options of PreProcessor_ClearTempFiles to be cObjects (fixes #34264)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandler/trunk@58495 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/PreProcessor/Tx_Formhandler_PreProcessor_ClearTempFiles.php
+++ Classes/PreProcessor/Tx_Formhandler_PreProcessor_ClearTempFiles.php
@@ -39,10 +39,10 @@ class Tx_Formhandler_PreProcessor_ClearTempFiles extends Tx_Formhandler_Abstract
 	 * @return array The probably modified GET/POST parameters
 	 */
 	public function process() {
-		$this->olderThanValue = $this->settings['clearTempFilesOlderThan.']['value'];
-		$this->olderThanUnit = $this->settings['clearTempFilesOlderThan.']['unit'];
-		if (!empty($this->olderThanValue) && is_numeric($this->olderThanValue)) {
-			$this->clearTempFiles($this->olderThanValue, $this->olderThanUnit);
+		$olderThanValue = $this->utilityFuncs->getSingle($this->settings['clearTempFilesOlderThan.'], 'value');
+		$olderThanUnit = $this->utilityFuncs->getSingle($this->settings['clearTempFilesOlderThan.'], 'unit');
+		if (strlen($olderThanValue) > 0 && is_numeric($olderThanValue)) {
+			$this->clearTempFiles($olderThanValue, $olderThanUnit);
 		}
 		return $this->gp;
 	}
