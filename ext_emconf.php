29ffd78bce82ce8ffb62b9c52b03ef2ed29d042c
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2016-01-29 15:23:09 +0000

    [TASK] Update PHP and TYPO3 dependency
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandler/branches/typo38-compat@90577 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,8 +31,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.5.0-0.0.0',
-			'typo3' => '7.6.0-7.6.99',
+			'php' => '7.0.0-0.0.0',
+			'typo3' => '8.0.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
