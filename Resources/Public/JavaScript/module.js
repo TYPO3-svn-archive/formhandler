3f8a08de8e18c95fdd25f016d77755723f9c79e1
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2016-01-29 09:27:31 +0000

    Changed jQuery to TYPO3.jQuery in module.js
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/formhandler/branches/typo37-compat@90561 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/JavaScript/module.js
+++ Resources/Public/JavaScript/module.js
@@ -1,50 +1,50 @@
-jQuery(function() {
+TYPO3.jQuery(function() {
 
 	function getSelectedUids(el) {
 		var selected = new Array();
 		if(typeof el !== 'undefined' && el.attr("data-uid") && el.attr("data-uid").length > 0) {
 			selected.push(el.attr("data-uid"));
 		} else {
-			jQuery('#formhandler-module .mark-row:checked').each(function() {
+			TYPO3.jQuery('#formhandler-module .mark-row:checked').each(function() {
 				selected.push(jQuery(this).attr("value"));
 			});
 		}
 		return selected.join(",");
 	}
-	jQuery("#formhandler-module .filterForm .select-all-pages").on("click", function() {
-		jQuery(this).closest('.input-group').find('INPUT.form-control').val("");
+	TYPO3.jQuery("#formhandler-module .filterForm .select-all-pages").on("click", function() {
+		TYPO3.jQuery(this).closest('.input-group').find('INPUT.form-control').val("");
 	});
-	jQuery("#formhandler-module A.select-all").on("click", function(e) {
+	TYPO3.jQuery("#formhandler-module A.select-all").on("click", function(e) {
 		e.preventDefault();
-		var allCheckboxes = jQuery('#formhandler-module .mark-row');
-		var activeCheckboxes = jQuery('#formhandler-module .mark-row:checked');
+		var allCheckboxes = TYPO3.jQuery('#formhandler-module .mark-row');
+		var activeCheckboxes = TYPO3.jQuery('#formhandler-module .mark-row:checked');
 		if(allCheckboxes.length === activeCheckboxes.length) {
 			allCheckboxes.prop("checked", false);
 		} else {
 			allCheckboxes.prop("checked", true);
 		}
 	});
-	jQuery("#formhandler-module .process-selected-actions A.pdf").on("click", function(e) {
+	TYPO3.jQuery("#formhandler-module .process-selected-actions A.pdf").on("click", function(e) {
 		e.preventDefault();
 		var selectedUids = getSelectedUids();
 		if(selectedUids.length > 0) {
-			jQuery('#process-selected-form-export INPUT.filetype').attr("value", "pdf");
-			jQuery('#process-selected-form-export INPUT.logDataUids').attr("value", selectedUids);
-			jQuery('#process-selected-form-export').submit();
+			TYPO3.jQuery('#process-selected-form-export INPUT.filetype').attr("value", "pdf");
+			TYPO3.jQuery('#process-selected-form-export INPUT.logDataUids').attr("value", selectedUids);
+			TYPO3.jQuery('#process-selected-form-export').submit();
 		}
 	});
-	jQuery("#formhandler-module .process-selected-actions A.csv").on("click", function(e) {
+	TYPO3.jQuery("#formhandler-module .process-selected-actions A.csv").on("click", function(e) {
 		e.preventDefault();
 		var selectedUids = getSelectedUids();
 		if(selectedUids.length > 0) {
-			jQuery('#process-selected-form-export INPUT.filetype').attr("value", "csv");
-			jQuery('#process-selected-form-export INPUT.logDataUids').attr("value", selectedUids);
-			jQuery('#process-selected-form-export').submit();
+			TYPO3.jQuery('#process-selected-form-export INPUT.filetype').attr("value", "csv");
+			TYPO3.jQuery('#process-selected-form-export INPUT.logDataUids').attr("value", selectedUids);
+			TYPO3.jQuery('#process-selected-form-export').submit();
 		}
 	});
-	jQuery("#formhandler-module A.delete").on("click", function(e) {
+	TYPO3.jQuery("#formhandler-module A.delete").on("click", function(e) {
 		e.preventDefault();
-		var infoElement = jQuery(this).find('SPAN.delete-info'); 
+		var infoElement = TYPO3.jQuery(this).find('SPAN.delete-info');
 		var selectedUids = getSelectedUids(infoElement);
 		if(selectedUids.length > 0) {
 			var modal = top.TYPO3.Modal.confirm(infoElement.data('title'), infoElement.data('message'), top.TYPO3.Severity.warning, [
@@ -64,8 +64,8 @@ jQuery(function() {
 					top.TYPO3.Modal.dismiss();
 				} else if (e.target.name === 'delete') {
 					top.TYPO3.Modal.dismiss();
-					jQuery('#process-selected-form-delete INPUT.logDataUids').attr("value", selectedUids);
-					jQuery('#process-selected-form-delete').submit();
+					TYPO3.jQuery('#process-selected-form-delete INPUT.logDataUids').attr("value", selectedUids);
+					TYPO3.jQuery('#process-selected-form-delete').submit();
 				}
 			});
 		}
